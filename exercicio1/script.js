b83cc7f8a2553070e709101df7e226428574f838
laisrmacedo <laisrodriguesmacedo@gmail.com>
2022-08-30 16:56:00 +0200

    Resoluçao exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+let numeroPar = Number(prompt("Digite um número par:"));
+
+console.log("O resto da divisão por 2 é:", numeroPar % 2);
+
+// O padrão é que todos os restos são iguais a 0, 
+//já que é um número par dividido por outro número par.
+
+// O resto da divisão será 1. 
