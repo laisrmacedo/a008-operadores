faa7ecbac3acfa957e37e143e39c336ae052ba65
laisrmacedo <laisrodriguesmacedo@gmail.com>
2022-08-30 17:23:15 +0200

    Resoluçao exercicio 3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,19 +1,11 @@
-// a) 5 é maior que 20 e também é menor que 2;
-
-// b) 5 é igual a 5 ou é igual a “5”;
-
-// c) negação de (vinte é maior que cinquenta)
-
-// d) negação de (vinte é maior que cinquenta OU cinquenta é maior que sessenta)
-
-let operacaoA = 5 > 20 && 5 < 2;
+let operacaoA = 5 > 20 && 5 < 2; // false && false --> false
 console.log(operacaoA);
 
-let operacaoB = 5 === 5 || 5 === "5";
+let operacaoB = 5 === 5 || 5 === "5"; // true || false --> true
 console.log(operacaoB)
 
-let operacaoC = !(20 > 50)
+let operacaoC = !(20 > 50) // !false --> true
 console.log(operacaoC)
 
-let operacaoD = !(20 > 50 || 50 > 60)
+let operacaoD = !(20 > 50 || 50 > 60) // !(false || false) --> !false --> true
 console.log(operacaoD)
